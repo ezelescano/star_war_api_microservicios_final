5b5d2ed5ab2e4390536e06581c2703357fb88fc8
ezelescano <ezequiel.lesca@gmail.com>
2023-11-06 21:29:37 -0300

    creando metodos estaticos

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -3,14 +3,13 @@ const {MONGO_URI} = require('../config/envs')
 
 const conn = mongoose.createConnection(MONGO_URI);
 
-const Character = conn.model('Character', require('./schemas/characterSchema'));
 
-const Film = conn.model('Film', require('./schemas/filmSchema'));
-
-const Planet = conn.model('Planet', require('./schemas/planetSchema'));
-
-Character.find().then((res) => console.log(res));
 
 module.exports ={
-    
+   
+    Character: conn.model('Character', require('./schemas/characterSchema')),
+
+    Film: conn.model('Film', require('./schemas/filmSchema')),
+
+    Planet: conn.model('Planet', require('./schemas/planetSchema'))
 }
