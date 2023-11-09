6a7c878546bdc26c0a6f8d6f99bac494a71cfc66
ezelescano <ezequiel.lesca@gmail.com>
2023-11-09 17:12:37 -0300

    validacion de modelo

--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const store = require('../database');
+const { validationModel} = require('../middleware')
 const router = Router();
 
 
-router.get('/:model', async (req, res)=> {
+router.get('/:model', validationModel, async (req, res)=> {
     const {model} = req.params;
     const response = await store[model].list();
     res.status(200).json(response);
